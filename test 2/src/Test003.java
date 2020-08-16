3f62b206c3c1e89724ed316a2d8f01f05d489052
15712 <15712@laptop1>
2020-08-16 19:56:47 -0400

    this is my first test

---
+++ test 2/src/Test003.java
@@ -0,0 +1,21 @@
+
+public class Test003 {
+
+	public static void main(String[] args) {
+		int i = 20;
+		int c =50;
+		do {System.out.println("starting value of i is " + i);
+		i-- ;
+		}
+		while (i >= 100) ;
+		}
+			
+
+		
+		}
+		
+	
+        
+	
+
+
